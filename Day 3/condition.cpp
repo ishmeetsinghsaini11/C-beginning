0a1b24862f6c1eaf483fffd5ef518fa034931e3a
Ishmeet Singh Saini <124482770+ishmeetsinghsaini11@users.noreply.github.com>
2023-02-12 06:01:19 +0000

    n

---
+++ Day 3/condition.cpp
@@ -0,0 +1,17 @@
+//conditon statements
+
+//if else condition
+#include<iostream>
+using namespace std;
+
+int main(){
+  int a,b;
+  cin>>a>>b;
+  
+  if(a>b){
+    cout<< "a="<< a << " is greater\n"<<endl;
+  }
+  else{
+    cout<<"b="<< b <<" is greater\n"<<endl;
+  }
+} 
