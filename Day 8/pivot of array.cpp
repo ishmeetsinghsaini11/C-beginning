0aea0720c4ef95e556361d92f446148a14720b7a
Ishmeet Singh Saini <124482770+ishmeetsinghsaini11@users.noreply.github.com>
2023-02-24 07:41:02 +0000

    N

---
+++ Day 8/pivot of array.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int pivot(int arr[],int size){
+    int s=0,e=size-1;
+    int mid=s+(e-s)/2;
+    
+    while(s<e){
+        if(arr[mid]>=arr[0]){
+            s=mid+1;
+        }
+        else{
+            e=mid;
+        }
+        mid = s+(e-s)/2;
+    }
+    return s;
+    
+}
+
+int main() {
+    int arr[5]={8,10,17,1,3};
+    
+    int index = pivot(arr,5);
+    cout<<"pivot is "<<index<<endl;
+
+    return 0;
+}
